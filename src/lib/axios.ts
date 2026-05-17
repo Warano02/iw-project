c3540f97b5eed6a028b43b54d3ea2963f209b667
Ulrich Ngachou <ulrichlandry722@gmail.com>
2026-05-17 20:29:39 +0100

    modification de create-admin et create-user

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_API_BASE_URL || "http://192.168.43.150:3000/",
+  baseURL: process.env.NEXT_API_BASE_URL || "http://192.168.43.150:3000",
   withCredentials: true,
 });
 
